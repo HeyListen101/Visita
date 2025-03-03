eec713c632f2aaf90a284b25e9155275c1642042
You Mu <21-1-00398@vsu.edu.ph>
2025-03-03 18:48:23 +0800

    Updated Landing Page

--- src/client/pages/LandingPage.jsx
+++ src/client/pages/LandingPage.jsx
@@ -1,8 +1,14 @@
+import backgroundImage from "../assets/background-images/LandingPage.png"
 import { Link } from "react-router-dom";
 
 function LandingPage() {
     return (
-        <div className="h-screen w-screen bg-[#0D1224] flex flex-col justify-center items-center">
+        <div 
+          className="h-screen w-screen bg-cover bg-center flex flex-col justify-center items-center text-white"
+          style={{
+            backgroundImage:`url(${backgroundImage})`
+          }}
+        >
           <h1 className="h1">Welcome to ViSiTa</h1>
           <p className="p">Go to the market, without going!</p>
           <Link to="/map">
