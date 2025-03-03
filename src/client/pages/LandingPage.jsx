a0235fed7186a6654487c66166b98788e8eb23a8
You Mu <21-1-00398@vsu.edu.ph>
2025-03-03 21:23:02 +0800

    Updated Styling
    Added the styling in the landing page and map page
    
    User Authentication & Interactive Map docs

--- src/client/pages/LandingPage.jsx
+++ src/client/pages/LandingPage.jsx
@@ -1,23 +1,61 @@
-import backgroundImage from "../assets/background-images/LandingPage.png"
+import backgroundImage from "../assets/background-images/LandingPage.png";
 import { Link } from "react-router-dom";
 
 function LandingPage() {
-    return (
-        <div 
-          className="h-screen w-screen bg-cover bg-center flex flex-col justify-center items-center text-white"
-          style={{
-            backgroundImage:`url(${backgroundImage})`
-          }}
-        >
-          <h1 className="h1">Welcome to ViSiTa</h1>
-          <p className="p">Go to the market, without going!</p>
-          <Link to="/map">
-            <button className="button">
-                Open Map
-            </button>
-          </Link>
+  return (
+    <div
+      className="fixed inset-0 flex items-center justify-center bg-cover bg-center overflow-hidden"
+      style={{ backgroundImage: "url('https://www.eduopinions.com/wp-content/uploads/2017/09/Visayas-State-University-VSU-campus.jpg')" }}
+    >
+      <div className="bg-white p-8 rounded-xl shadow-lg w-96 flex flex-col items-center">
+        <h2 className="text-xl font-bold mb-4 text-black">Log In</h2>
+
+        {/* Styled Inputs */}
+        <input
+          type="email"
+          placeholder="Email address"
+          className="w-full p-2 mb-3 border border-gray-400 rounded-md focus:border-blue-600 focus:outline-blue-600"
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          className="w-full p-2 mb-3 border border-gray-400 rounded-md focus:border-blue-600 focus:outline-blue-600"
+        />
+
+        {/* Sign-up and Forgot Password Links */}
+        <div className="flex justify-between w-full text-sm mb-4">
+          <a href="#" className="text-[#696047] hover:text-[#57503A]">
+            Sign-up or Register
+          </a>
+          <a href="#" className="text-[#696047] hover:text-[#57503A]">
+            Forgot Password?
+          </a>
         </div>
-      );
+
+        {/* Continue Button */}
+        <Link to="/map" className="block w-full">
+          <button className="w-full text-white p-2 rounded-md">
+            Continue
+          </button>
+        </Link>
+
+        {/* OR Divider with Color Matching */}
+        <div className="flex items-center w-full my-3">
+          <hr className="flex-grow border-t border-[#696047] mx-2" />
+          <span className="text-[#696047] font-medium text-m relative bottom-[3px]">or</span>
+          <hr className="flex-grow border-t border-[#696047] mx-2" />
+        </div>
+
+        {/* Google Sign-In Button */}
+        <Link to="/map" className="block w-full">
+        <button className="w-full flex items-center justify-center text-white">
+            <img src="/path-to-google-logo.png" alt="Google" className="w-5 h-5 mr-2" />
+            Continue with Google
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
 }
 
 export default LandingPage;
--- src/client/pages/MapPage.jsx
+++ src/client/pages/MapPage.jsx
@@ -1,17 +1,113 @@
-import { Link } from "react-router-dom";
+import React from "react";
 
-function MapPage() {
-    return (
-        <div className="h-screen w-screen bg-[#0D1224] flex flex-col justify-center items-center">
-          <h1 className="h1">This is where the map will be!</h1>
-          <p className="p">Alongside the real time chat</p>
-          <Link to="/">
-            <button className="button">
-                Open Map
-            </button>
-          </Link>
-        </div>
-      );
-}
+const InteractiveMap = () => {
+  return (
+    <div className="relative w-[1440px] h-[1024px] bg-white">
+      {/* Top Bar */}
+      <div className="absolute w-full h-[90px] bg-gray-200" />
 
-export default MapPage;
+      {/* Search Bar */}
+      <div className="absolute w-[500px] h-[50px] bg-white shadow-md rounded-lg left-1/2 transform -translate-x-1/2 top-[20px]" />
+
+      {/* Search Icon */}
+      <div className="absolute w-[20px] h-[20px] left-[495px] top-[35px]" />
+
+      {/* Profile Icon */}
+      <div className="absolute w-[50px] h-[50px] bg-white shadow-md left-[1365px] top-[20px]" />
+
+      {/* Chat Community */}
+      <div className="absolute w-[200px] h-[50px] bg-white shadow-md rounded-lg left-[25px] top-[949px] flex items-center justify-center">
+        <span className="text-[16px] font-semibold text-black">Chat Community</span>
+      </div>
+
+      {/* Buttons */}
+      {[622, 664, 706, 580, 538, 455, 420, 385, 350, 315, 280, 245, 210].map((top, index) => (
+        <div
+          key={index}
+          className="absolute w-[64px] h-[42px] left-[1351px] border border-white rounded-md"
+          style={{ top: `${top}px`, backgroundColor: index % 2 === 0 ? "#8AD70E" : "#F07474" }}
+        />
+      ))}
+      {/* Rectangle 252 */}
+      <div className="absolute w-[683.48px] h-[35px] left-[638.08px] top-[232.83px] bg-gray-400/40 rounded-tr-xl"></div>
+
+      {/* Rectangle 253-256 */}
+      <div className="absolute w-[344.3px] h-[14px] left-[798.08px] top-[710.27px] bg-gray-400/40"></div>
+      <div className="absolute w-[111.35px] h-[14px] left-[1155.17px] top-[710.27px] bg-gray-400/40"></div>
+      <div className="absolute w-[344.3px] h-[14px] left-[798.08px] top-[839.08px] bg-gray-400/40"></div>
+
+      {/* Rectangle 257 */}
+      <div className="absolute w-[32.16px] h-[673.99px] left-[1289.49px] top-[267.23px] bg-gradient-to-b from-gray-300 to-white"></div>
+
+      {/* Buttons */}
+      {[
+        { left: 813, top: 733, bg: "bg-green-700" },
+        { left: 813.43, top: 781.67, bg: "bg-lime-500" },
+        { left: 858, top: 782, bg: "bg-lime-400" },
+        { left: 903, top: 782, bg: "bg-green-300" },
+        { left: 948, top: 782, bg: "bg-green-900" },
+        { left: 993, top: 782, bg: "bg-green-700" },
+        { left: 1037, top: 782, bg: "bg-red-400" },
+        { left: 1082, top: 782, bg: "bg-red-400" },
+        { left: 858.23, top: 732.67, bg: "bg-lime-500" },
+        { left: 903, top: 733, bg: "bg-lime-400" },
+        { left: 992.62, top: 732.67, bg: "bg-red-400" },
+        { left: 1037, top: 733, bg: "bg-green-700 w-[89.59px]" },
+        { left: 948, top: 733, bg: "bg-green-300" },
+        { left: 424.34, top: 129.22, bg: "bg-green-700 w-[104.31px] h-[70.01px]" },
+        { left: 380, top: 129, bg: "bg-green-900 h-[70.01px]" },
+        { left: 335, top: 129, bg: "bg-green-300 h-[70.01px]" },
+        { left: 271, top: 129, bg: "bg-lime-400 w-[64px] h-[70.01px]" },
+        { left: 206.31, top: 129.22, bg: "bg-red-400 w-[64px] h-[70.01px]" },
+      ].map((btn, idx) => (
+        <div
+          key={idx}
+          className={`absolute ${btn.bg} border border-white rounded-md w-[44.8px] h-[49px]`}
+          style={{ left: `${btn.left}px`, top: `${btn.top}px` }}
+        ></div>
+      ))}
+
+      {/* Rectangle 315 */}
+      <div className="absolute w-16 h-[70px] left-[141px] top-[129px] bg-green-700 border border-white rounded-md" />
+      
+      {/* Rectangle 316 */}
+      <div className="absolute w-16 h-[35px] left-[76px] top-[129px] bg-green-900 border border-white rounded-md" />
+
+      {/* Rectangle 317 */}
+      <div className="absolute w-8 h-[105px] left-[25px] top-[164px] bg-green-300 border border-white rounded-md" />
+      
+      {/* Rectangle 318 - 321 */}
+      <div className="absolute w-8 h-[70px] left-[1005px] top-[129px] bg-green-700 border border-white rounded-md" />
+      <div className="absolute w-8 h-[70px] left-[973px] top-[129px] bg-green-900 border border-white rounded-md" />
+      <div className="absolute w-8 h-[70px] left-[941px] top-[129px] bg-green-300 border border-white rounded-md" />
+      <div className="absolute w-8 h-[70px] left-[909px] top-[129px] bg-lime-500 border border-white rounded-md" />
+      
+      {/* Rectangle 322 */}
+      <div className="absolute w-[141px] h-[70px] left-[1231px] top-[129px] bg-lime-600 rounded-md" />
+      
+      {/* Labels */}
+      <div className="absolute w-[199px] text-center text-black font-medium text-lg left-[calc(50%-198px/2-97px)] top-[99px]">
+        To VSU Lower Campus
+      </div>
+      
+      <div className="absolute w-[154px] text-center text-black font-medium text-lg left-[calc(50%-154px/2-98px)] top-[939px]">
+        Entrance/Exit (Exit for Vehicles)
+      </div>
+      
+      <div className="absolute w-[201px] text-center text-black font-medium text-lg left-[calc(50%-201px/2+586px)] top-[941px]">
+        Entrance/Exit (Entrance for Vehicles)
+      </div>
+      
+      {/* Road */}
+      <div className="absolute w-[525px] h-[11px] left-[82px] top-[209px] bg-gradient-to-r from-white/40 to-gray-400/40" />
+      
+      {/* Icons */}
+      <div className="absolute w-[15px] h-[17px] left-[1375px] top-[219px] bg-white" />
+      <div className="absolute w-[15px] h-[17px] left-[1376px] top-[254px] bg-white" />
+      <div className="absolute w-[15px] h-[17px] left-[1376px] top-[289px] bg-white" />
+      
+    </div>
+  );
+};
+
+export default InteractiveMap;
