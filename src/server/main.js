e563ecc4c947a972d028ebcc2a0eac9267e76a8b
You Mu <21-1-00398@vsu.edu.ph>
2025-03-04 18:07:23 +0800

    Added environment variables and reconfigured necessary start up files

--- src/server/main.js
+++ src/server/main.js
@@ -1,12 +1,24 @@
 import express from "express";
 import ViteExpress from "vite-express";
+import dotenv from "dotenv";
+import path from "path";
+
+const ENV = process.env.NODE_ENV || 'dev';
+
+const envFile = {
+  dev: "dev.env",
+  test: "test.env",
+  prod: "prod.env",
+}[ENV];
+
+dotenv.config({ path: path.resolve(process.cwd(), envFile)})
 
 const app = express();
 
 app.get("/hello", (req, res) => {
-  res.send("Hello Vite + React!");
+  res.send("Visita!");
 });
 
 ViteExpress.listen(app, 3000, () =>
-  console.log("Server is listening on port 3000..."),
+  console.log("Server has served on port 3000!"),
 );
